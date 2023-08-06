e54cee92f38e67ca99d5aa10b254ea35e855af9d
Eldos <eldos.baktybekov@gmail.com>
2023-08-06 14:20:42 +0600

    :sparkles: Add all media types support

--- secret_messages/models.py
+++ secret_messages/models.py
@@ -37,6 +37,12 @@ class SecretMedia(models.Model):
     class MediaType(models.IntegerChoices):
         PHOTO = 1
         VOICE = 2
+        VIDEO = 3
+        AUDIO = 4
+        ANIMATION = 5
+        DOCUMENT = 6
+        VIDEO_NOTE = 7
+        STICKER = 8
 
     id = models.UUIDField(primary_key=True, default=uuid4)
     name = models.CharField(max_length=64, null=True, blank=True)
