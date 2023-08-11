609cdf46df9b8f044684047fcecb21fff69f1362
Eldos <eldos.baktybekov@gmail.com>
2023-08-11 09:51:58 +0600

    :card_file_box: Merge all secret message preferences into one SecretMessageTheme model

--- secret_messages/models/secret_message_templates.py
+++ secret_messages/models/secret_message_templates.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.db.models import CheckConstraint
 
-__all__ = ('SecretMessageDescriptionTemplate', 'SecretMessageButtonTemplate')
+__all__ = ('SecretMessageTheme',)
 
 
-class SecretMessageDescriptionTemplate(models.Model):
-    text = models.CharField(max_length=200)
+class SecretMessageTheme(models.Model):
+    description_template_text = models.CharField(max_length=200)
+    button_text = models.CharField(max_length=32)
+    description_emoji = models.CharField(max_length=16)
+    button_emoji = models.CharField(max_length=16)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
@@ -13,10 +16,8 @@ class SecretMessageDescriptionTemplate(models.Model):
             CheckConstraint(
                 name='secret_message_template_contains_name',
                 check=models.Q(text__contains='{name}'),
+                violation_error_message=(
+                    'Secret message template must contain "{name}"'
+                ),
             ),
         )
-
-
-class SecretMessageButtonTemplate(models.Model):
-    text = models.CharField(max_length=32)
-    created_at = models.DateTimeField(auto_now_add=True)
