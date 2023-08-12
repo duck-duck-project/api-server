7f012009a3537f58f0962dc2f0eb320c667a5695
Eldos <eldos.baktybekov@gmail.com>
2023-08-12 23:23:40 +0600

    :sparkles: Add secret message themes repository

--- telegram_bot/src/secret_messaging/repositories/__init__.py
+++ telegram_bot/src/secret_messaging/repositories/__init__.py
@@ -1,4 +1,5 @@
 from .contacts import *
 from .secret_medias import *
+from .secret_message_themes import *
 from .secret_messages import *
 from .users import *
---
+++ telegram_bot/src/secret_messaging/repositories/secret_message_themes.py
@@ -0,0 +1,25 @@
+from pydantic import TypeAdapter
+
+from src.common.repositories import APIRepository
+from src.secret_messaging.models import SecretMessageTheme
+
+__all__ = ('SecretMessageThemeRepository',)
+
+
+class SecretMessageThemeRepository(APIRepository):
+
+    async def get_all(self) -> list[SecretMessageTheme]:
+        url = '/secret-message-themes/'
+        async with self._http_client.get(url) as response:
+            response_data: list[dict] = await response.json()
+        type_adapter = TypeAdapter(list[SecretMessageTheme])
+        return type_adapter.validate_python(response_data)
+
+    async def get_by_id(
+            self,
+            secret_message_theme_id: int,
+    ) -> SecretMessageTheme:
+        url = f'/secret-message-themes/{secret_message_theme_id}/'
+        async with self._http_client.get(url) as response:
+            response_data: dict = await response.json()
+        return SecretMessageTheme.model_validate(response_data)
