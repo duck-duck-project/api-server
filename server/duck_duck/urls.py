020263dfad3422520e96762023ba12f4089d5b1f
Eldos <eldos.baktybekov@gmail.com>
2023-08-13 10:42:17 +0600

    :pencil2: Resolve url paths

--- server/duck_duck/urls.py
+++ server/duck_duck/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
+    path('', include('users.urls')),
     path('', include('secret_messages.urls')),
 ]
