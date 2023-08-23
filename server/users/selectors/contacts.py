06d2977cd606b1d4a321eb9da4f733523758ba0a
Eldos <eldos.baktybekov@gmail.com>
2023-08-23 14:42:42 +0600

    :necktie: Retrieve only not deleted contacts in contact selectors

--- server/users/selectors/contacts.py
+++ server/users/selectors/contacts.py
@@ -4,45 +4,12 @@ from users.exceptions import ContactDoesNotExistError
 from users.models import Contact
 
 __all__ = (
-    'get_contact_by_user_ids',
-    'get_contact_by_id',
-    'get_contacts_by_user_id',
+    'get_not_deleted_contact_by_id',
+    'get_not_deleted_contacts_by_user_id',
 )
 
 
-def get_contact_by_user_ids(
-        *,
-        of_user_id: int,
-        to_user_id: int,
-) -> Contact:
-    """Retrieve contact instance by user IDs.
-
-    Args:
-        of_user_id: ID of user who adds contact.
-        to_user_id: ID of user who is added to contacts.
-
-    Returns:
-        Contact instance if exists.
-
-    Raises:
-        ContactDoesNotExistError: If contact does not exist.
-    """
-    try:
-        return (
-            Contact.objects
-            .select_related(
-                'of_user',
-                'to_user',
-                'of_user__secret_message_theme',
-                'to_user__secret_message_theme',
-            )
-            .get(of_user_id=of_user_id, to_user_id=to_user_id)
-        )
-    except Contact.DoesNotExist:
-        raise ContactDoesNotExistError
-
-
-def get_contact_by_id(contact_id: int) -> Contact:
+def get_not_deleted_contact_by_id(contact_id: int) -> Contact:
     """Retrieve contact instance by ID.
 
     Args:
@@ -63,14 +30,14 @@ def get_contact_by_id(contact_id: int) -> Contact:
                 'of_user__secret_message_theme',
                 'to_user__secret_message_theme',
             )
-            .get(id=contact_id)
+            .get(id=contact_id, is_deleted=False)
         )
     except Contact.DoesNotExist:
         raise ContactDoesNotExistError
 
 
-def get_contacts_by_user_id(user_id: int) -> QuerySet[Contact]:
-    """Retrieve contacts of user.
+def get_not_deleted_contacts_by_user_id(user_id: int) -> QuerySet[Contact]:
+    """Retrieve contacts of user that are not marked as deleted.
 
     Args:
         user_id: ID of user.
@@ -86,5 +53,5 @@ def get_contacts_by_user_id(user_id: int) -> QuerySet[Contact]:
             'of_user__secret_message_theme',
             'to_user__secret_message_theme',
         )
-        .filter(of_user_id=user_id)
+        .filter(of_user_id=user_id, is_deleted=False)
     )
