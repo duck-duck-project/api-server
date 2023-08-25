b30139840d10ef34d5d5d015f3836a692044aa0c
Eldos <eldos.baktybekov@gmail.com>
2023-08-25 19:29:31 +0600

    :recycle: Refactor contacts tests using factory-boy

--- server/users/tests/test_contacts/test_views.py
+++ server/users/tests/test_contacts/test_views.py
@@ -3,34 +3,28 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 
 from users.models import User, Contact
+from users.tests.test_contacts.factories import ContactFactory
+from users.tests.test_users.factories import UserFactory
 
 
 class ContactCreateApiTests(APITestCase):
 
     def setUp(self) -> None:
-        self.eldos = User.objects.create(
-            id=123456789,
-            username='usbtypec',
-            fullname='Eldos',
-        )
-        self.alex = User.objects.create(
-            id=987654321,
-            username='pushkin',
-            fullname='Alexander Pushkin',
-        )
+        self.user_1 = UserFactory()
+        self.user_2 = UserFactory()
 
     def test_create_contact(self) -> None:
         url = reverse('users:contacts-create')
         data = {
-            'of_user_id': self.eldos.id,
-            'to_user_id': self.alex.id,
+            'of_user_id': self.user_1.id,
+            'to_user_id': self.user_2.id,
             'private_name': 'Alex',
             'public_name': 'Alexander',
         }
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(response.data['of_user']['id'], self.eldos.id)
-        self.assertEqual(response.data['to_user']['id'], self.alex.id)
+        self.assertEqual(response.data['of_user']['id'], self.user_1.id)
+        self.assertEqual(response.data['to_user']['id'], self.user_2.id)
         self.assertIn('id', response.data)
 
         contact = (
@@ -39,8 +33,8 @@ class ContactCreateApiTests(APITestCase):
             .get(id=response.data['id'])
         )
 
-        self.assertEqual(contact.of_user, self.eldos)
-        self.assertEqual(contact.to_user, self.alex)
+        self.assertEqual(contact.of_user, self.user_1)
+        self.assertEqual(contact.to_user, self.user_2)
         self.assertEqual(contact.private_name, 'Alex')
         self.assertEqual(contact.public_name, 'Alexander')
         self.assertFalse(contact.is_hidden)
@@ -49,7 +43,7 @@ class ContactCreateApiTests(APITestCase):
         url = reverse('users:contacts-create')
         data = {
             'of_user_id': 12345,
-            'to_user_id': self.alex.id,
+            'to_user_id': self.user_2.id,
             'private_name': 'Alex',
             'public_name': 'Alexander',
         }
@@ -60,37 +54,27 @@ class ContactCreateApiTests(APITestCase):
 class ContactCreateSoftDeletedContactTests(APITestCase):
 
     def setUp(self) -> None:
-        self.eldos = User.objects.create(
-            id=123456789,
-            username='usbtypec',
-            fullname='Eldos',
-        )
-        self.alex = User.objects.create(
-            id=987654321,
-            username='pushkin',
-            fullname='Alexander Pushkin',
-        )
-        self.contact = Contact.objects.create(
-            of_user=self.eldos,
-            to_user=self.alex,
-            private_name='Alex',
-            public_name='Alexander',
-            is_deleted=True,
-        )
+        self.contact = ContactFactory()
 
     def test_create_contact_mark_as_not_deleted(self) -> None:
         url = reverse('users:contacts-create')
         data = {
-            'of_user_id': self.eldos.id,
-            'to_user_id': self.alex.id,
+            'of_user_id': self.contact.of_user.id,
+            'to_user_id': self.contact.to_user.id,
             'private_name': 'Alex',
             'public_name': 'Alexander',
         }
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data['id'], self.contact.id)
-        self.assertEqual(response.data['of_user']['id'], self.eldos.id)
-        self.assertEqual(response.data['to_user']['id'], self.alex.id)
+        self.assertEqual(
+            response.data['of_user']['id'],
+            self.contact.of_user.id,
+        )
+        self.assertEqual(
+            response.data['to_user']['id'],
+            self.contact.to_user.id,
+        )
         self.assertEqual(response.data['private_name'], 'Alex')
         self.assertEqual(response.data['public_name'], 'Alexander')
         self.assertFalse(response.data['is_hidden'])
@@ -152,22 +136,7 @@ class ContactUpdateApiTests(APITestCase):
 class ContactDeleteApiTests(APITestCase):
 
     def setUp(self) -> None:
-        self.eldos = User.objects.create(
-            id=123456789,
-            username='usbtypec',
-            fullname='Eldos',
-        )
-        self.alex = User.objects.create(
-            id=987654321,
-            username='pushkin',
-            fullname='Alexander Pushkin',
-        )
-        self.contact = Contact.objects.create(
-            of_user=self.eldos,
-            to_user=self.alex,
-            private_name='Alex',
-            public_name='Alexander',
-        )
+        self.contact = ContactFactory()
 
     def test_delete_contact(self) -> None:
         url = reverse(
@@ -190,22 +159,7 @@ class ContactDeleteApiTests(APITestCase):
 class ContactRetrieveApiTests(APITestCase):
 
     def setUp(self) -> None:
-        self.eldos = User.objects.create(
-            id=123456789,
-            username='usbtypec',
-            fullname='Eldos',
-        )
-        self.alex = User.objects.create(
-            id=987654321,
-            username='pushkin',
-            fullname='Alexander Pushkin',
-        )
-        self.contact = Contact.objects.create(
-            of_user=self.eldos,
-            to_user=self.alex,
-            private_name='Alex',
-            public_name='Alexander',
-        )
+        self.contact = ContactFactory()
 
     def test_retrieve_user_by_id(self) -> None:
         url = reverse(
@@ -215,10 +169,19 @@ class ContactRetrieveApiTests(APITestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['id'], self.contact.id)
-        self.assertEqual(response.data['of_user']['id'], self.eldos.id)
-        self.assertEqual(response.data['to_user']['id'], self.alex.id)
-        self.assertEqual(response.data['private_name'], 'Alex')
-        self.assertEqual(response.data['public_name'], 'Alexander')
+        self.assertEqual(
+            response.data['of_user']['id'],
+            self.contact.of_user.id,
+        )
+        self.assertEqual(
+            response.data['to_user']['id'],
+            self.contact.to_user.id,
+        )
+        self.assertEqual(
+            response.data['private_name'],
+            self.contact.private_name,
+        )
+        self.assertEqual(response.data['public_name'], self.contact.public_name)
         self.assertFalse(response.data['is_hidden'])
 
     def test_retrieve_user_does_not_exist(self) -> None:
@@ -234,36 +197,12 @@ class ContactRetrieveApiTests(APITestCase):
 class UserContactsListApiTests(APITestCase):
 
     def setUp(self) -> None:
-        self.eldos = User.objects.create(
-            id=123456789,
-            username='usbtypec',
-            fullname='Eldos',
-        )
-        self.alex = User.objects.create(
-            id=987654321,
-            username='pushkin',
-            fullname='Alexander Pushkin',
-        )
-        self.shahadat = User.objects.create(
-            id=111111111,
-            username=None,
-            fullname='Shahadat',
-        )
-        self.contact1 = Contact.objects.create(
-            of_user=self.eldos,
-            to_user=self.alex,
-            private_name='Alex',
-            public_name='Alexander',
-        )
-        self.contact2 = Contact.objects.create(
-            of_user=self.eldos,
-            to_user=self.shahadat,
-            private_name='Shahadat',
-            public_name='Shahadat',
-        )
+        self.user = UserFactory()
+        self.contact_1 = ContactFactory(of_user=self.user)
+        self.contact_2 = ContactFactory(of_user=self.user)
 
     def test_get_contacts_list(self) -> None:
-        url = reverse('users:contacts-list', args=(self.eldos.id,))
+        url = reverse('users:contacts-list', args=(self.user.id,))
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 2)
