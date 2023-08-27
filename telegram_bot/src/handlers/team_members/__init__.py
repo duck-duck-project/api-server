e8708cbfc40876e3beffb24ee22390cf1e82a24c
Eldos <eldos.baktybekov@gmail.com>
2023-08-27 21:15:10 +0600

    :sparkles: Add team member detail handlers

--- telegram_bot/src/handlers/team_members/__init__.py
+++ telegram_bot/src/handlers/team_members/__init__.py
@@ -1,6 +1,6 @@
 from aiogram import Dispatcher
 
-from . import list, create
+from . import list, create, detail
 
 __all__ = ('register_handlers',)
 
@@ -8,3 +8,4 @@ __all__ = ('register_handlers',)
 def register_handlers(dispatcher: Dispatcher) -> None:
     list.register_handlers(dispatcher)
     create.register_handlers(dispatcher)
+    detail.register_handlers(dispatcher)
