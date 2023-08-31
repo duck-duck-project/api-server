016dd04ab5f9dec8ca04b9eebe3fd491c817acba
Eldos <eldos.baktybekov@gmail.com>
2023-08-31 16:25:05 +0600

    :white_check_mark: Update tests related to user model

--- server/users/tests/test_users/factories.py
+++ server/users/tests/test_users/factories.py
@@ -11,3 +11,4 @@ class UserFactory(DjangoModelFactory):
     id = factory.Sequence(lambda n: n + 1000000)
     username = factory.Sequence(lambda n: f'username-{n}')
     fullname = factory.Sequence(lambda n: f'fullname-{n}')
+    born_at = factory.Faker('date')
