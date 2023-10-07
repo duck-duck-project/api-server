b9f7258ce5e4470fb0606c67467198a96ad47a1d
Eldos <eldos.baktybekov@gmail.com>
2023-10-07 16:47:36 +0600

    :sparkles: Add Telegram notifications about taxes and allowance

--- economics/services/taxes.py
+++ economics/services/taxes.py
@@ -1,5 +1,5 @@
 from economics.models import Transaction
-from economics.services import create_system_deposit
+from economics.services.withdrawal import create_system_withdrawal
 from users.models import User
 
 __all__ = ('tax_user',)
@@ -7,8 +7,8 @@ __all__ = ('tax_user',)
 
 def tax_user(*, user: User, balance: int) -> Transaction:
     amount_to_withdraw_as_tax = int(balance * 0.1)
-    return create_system_deposit(
+    return create_system_withdrawal(
         user=user,
         amount=amount_to_withdraw_as_tax,
-        description='Tax',
+        description='Налог на имущество',
     )
--- economics/tasks.py
+++ economics/tasks.py
@@ -1,6 +1,13 @@
 from celery import shared_task
+from django.conf import settings
 
 from economics.services import compute_user_balance, tax_user
+from economics.services.allowance import create_allowance
+from telegram.services import (
+    closing_telegram_http_client_factory,
+    TelegramBotService,
+    TransactionNotifier,
+)
 from users.models import User
 
 
@@ -8,10 +15,18 @@ from users.models import User
 def tax_users() -> None:
     users = User.objects.all()
 
-    for user in users:
-        user_balance = compute_user_balance(user)
+    with closing_telegram_http_client_factory(
+            token=settings.TELEGRAM_BOT_TOKEN,
+    ) as telegram_http_client:
+        telegram_bot_service = TelegramBotService(telegram_http_client)
+        transaction_notifier = TransactionNotifier(telegram_bot_service)
 
-        if user_balance < 50000:
-            continue
+        for user in users:
+            user_balance = compute_user_balance(user)
 
-        tax_user(user=user, balance=user_balance)
+            if user_balance < 1000:
+                deposit = create_allowance(user=user, balance=user_balance)
+                transaction_notifier.notify_deposit(deposit)
+            elif user_balance >= 1100:
+                withdrawal = tax_user(user=user, balance=user_balance)
+                transaction_notifier.notify_withdrawal(withdrawal)
