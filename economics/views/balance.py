63272bbf4dc2dcd2807ad2dc2933a54fa65324d1
Eldos <eldos.baktybekov@gmail.com>
2023-10-23 10:05:50 +0600

    :sparkles: Add richest users page in admin panel

--- economics/views/balance.py
+++ economics/views/balance.py
@@ -1,15 +1,23 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
 from rest_framework import serializers, status
 from rest_framework.exceptions import NotFound
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from economics.services import compute_user_balance
+from economics.services import (
+    compute_user_balance,
+    get_user_balances,
+    sort_richest_users,
+)
 from economics.tasks import send_richest_users
 from users.exceptions import UserDoesNotExistsError
 from users.selectors.users import get_user_by_id
 
-__all__ = ('BalanceRetrieveApi', 'RichestUsersStatisticsApi')
+__all__ = (
+    'BalanceRetrieveApi', 'RichestUsersStatisticsApi',
+    'RichestUsersStatisticsView')
 
 
 class BalanceRetrieveApi(APIView):
@@ -49,3 +57,20 @@ class RichestUsersStatisticsApi(APIView):
             limit=limit,
         )
         return Response(status=status.HTTP_202_ACCEPTED)
+
+
+class RichestUsersStatisticsView(LoginRequiredMixin, TemplateView):
+    template_name = 'admin_extensions/richest_users.html'
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        user_balances = get_user_balances()
+        richest_users = sort_richest_users(user_balances)
+        first_50_richest_users = richest_users[:50]
+        total_balance = sum(
+            user_balance.balance
+            for user_balance in first_50_richest_users
+        )
+        context_data['user_balances'] = first_50_richest_users
+        context_data['total_balance'] = total_balance
+        return context_data
