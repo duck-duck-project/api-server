66e5a047c6c09cf894cc1de57493d362d6c1e341
Eldos <eldos.baktybekov@gmail.com>
2023-12-31 18:30:26 +0600

    :sparkles: Refactor tax users task using DI

--- economics/tasks.py
+++ economics/tasks.py
@@ -1,7 +1,9 @@
 from celery import shared_task
 from django.conf import settings
 from django.db import transaction
+from fast_depends import Depends
 
+from economics.dependencies import get_transaction_notifier
 from economics.exceptions import InsufficientFundsForSystemWithdrawalError
 from economics.models import OperationPrice
 from economics.services import (
@@ -24,24 +26,22 @@ from users.selectors.users import get_user_by_id
 
 
 @shared_task
-def tax_users() -> None:
+def tax_users(
+        transaction_notifier: TransactionNotifier = (
+                Depends(get_transaction_notifier)
+        ),
+) -> None:
     users = User.objects.all()
 
-    with closing_telegram_http_client_factory(
-            token=settings.TELEGRAM_BOT_TOKEN,
-    ) as telegram_http_client:
-        telegram_bot_service = TelegramBotService(telegram_http_client)
-        transaction_notifier = TransactionNotifier(telegram_bot_service)
+    for user in users:
+        user_balance = compute_user_balance(user)
 
-        for user in users:
-            user_balance = compute_user_balance(user)
-
-            if user_balance < 1000:
-                deposit = create_allowance(user=user, balance=user_balance)
-                transaction_notifier.notify_deposit(deposit)
-            elif user_balance >= 1100:
-                withdrawal = tax_user(user=user, balance=user_balance)
-                transaction_notifier.notify_withdrawal(withdrawal)
+        if user_balance < 1000:
+            deposit = create_allowance(user=user, balance=user_balance)
+            transaction_notifier.notify_deposit(deposit)
+        elif user_balance >= 1100:
+            withdrawal = tax_user(user=user, balance=user_balance)
+            transaction_notifier.notify_withdrawal(withdrawal)
 
 
 @shared_task
