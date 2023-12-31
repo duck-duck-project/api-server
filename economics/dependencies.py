8970c714c179ab7ac008ced78528cf3495a781d0
Eldos <eldos.baktybekov@gmail.com>
2023-12-31 18:29:14 +0600

    :sparkles: Add transaction notifier factory

---
+++ economics/dependencies.py
@@ -0,0 +1,14 @@
+from fast_depends import Depends
+
+from telegram.dependencies import get_telegram_bot_service
+from telegram.services import TransactionNotifier, TelegramBotService
+
+__all__ = ('get_transaction_notifier',)
+
+
+def get_transaction_notifier(
+        telegram_bot_service: TelegramBotService = (
+                Depends(get_telegram_bot_service)
+        ),
+) -> TransactionNotifier:
+    return TransactionNotifier(telegram_bot_service)
