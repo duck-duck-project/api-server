2242ce7b4a33ecdaa5585ae7834c0ee509852ec1
Eldos <eldos.baktybekov@gmail.com>
2024-01-10 23:05:26 +0600

    :recycle: Refactor exceptions in transfer rollback service
    - Remove attributes of exceptions.
    - Populate exceptions with ready error messages.

--- economics/exceptions/insufficient_funds_for_transfer_rollback.py
+++ economics/exceptions/insufficient_funds_for_transfer_rollback.py
@@ -1,9 +1,5 @@
-from dataclasses import dataclass
-from uuid import UUID
-
 __all__ = ('InsufficientFundsForTransferRollbackError',)
 
 
-@dataclass(frozen=True, slots=True)
 class InsufficientFundsForTransferRollbackError(Exception):
-    transaction_id: UUID
+    pass
