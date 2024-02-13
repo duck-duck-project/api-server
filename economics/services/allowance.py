e934f1ada0fc8de88e1b385f301b52735890a7c3
Eldos <eldos.baktybekov@gmail.com>
2024-02-13 18:18:01 +0600

    :necktie: Add service for creating stipend for user

--- economics/services/allowance.py
+++ economics/services/allowance.py
@@ -1,8 +1,15 @@
+import random
+
+from django.db import transaction
+from fast_depends import Depends
+
+from economics.dependencies import get_transaction_notifier
 from economics.models import Transaction
 from economics.services.deposit import create_system_deposit
+from telegram.services import TransactionNotifier
 from users.models import User
 
-__all__ = ('create_allowance',)
+__all__ = ('create_allowance', 'create_stipend')
 
 
 def compute_allowance_amount(balance: int) -> int:
@@ -15,3 +22,22 @@ def create_allowance(*, user: User, balance: int) -> Transaction:
         amount=compute_allowance_amount(balance),
         description='Пособие по безработице',
     )
+
+
+@transaction.atomic
+def create_stipend(
+        user: User,
+        transaction_notifier: TransactionNotifier = Depends(
+            get_transaction_notifier,
+        )
+):
+    deposit = create_system_deposit(
+        user=user,
+        amount=random.choice([1, 2, 2, 3, 3, 3, 4, 4, 5]) * 1440,
+        description='Стипендия',
+    )
+    is_notification_delivered = transaction_notifier.notify_deposit(
+        deposit=deposit,
+    )
+    if not is_notification_delivered:
+        transaction.rollback()
