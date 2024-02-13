a172c7e826c55c95e4812f5d823f69ccf5f09bfb
Eldos <eldos.baktybekov@gmail.com>
2024-02-13 18:18:09 +0600

    :recycle: Refactor task

--- manas_id/tasks.py
+++ manas_id/tasks.py
@@ -1,26 +1,17 @@
-import random
+import contextlib
 
 from celery import shared_task
-from fast_depends import inject, Depends
+from fast_depends import inject
 
-from economics.dependencies import get_transaction_notifier
-from economics.services import create_system_deposit
+from economics.services.allowance import create_stipend
 from manas_id.selectors import iter_manas_ids
-from telegram.services import TransactionNotifier
 
 
 @shared_task
 @inject
 def give_away_stipends(
-        transaction_notifier: TransactionNotifier = Depends(
-                get_transaction_notifier,
-        )
 ) -> None:
     for manas_ids in iter_manas_ids():
         for manas_id in manas_ids:
-            deposit = create_system_deposit(
-                user=manas_id.user,
-                amount=random.randint(1000, 5000),
-                description='Стипендия'
-            )
-            transaction_notifier.notify_deposit(deposit)
+            with contextlib.suppress(Exception):
+                create_stipend(user=manas_id.user)
