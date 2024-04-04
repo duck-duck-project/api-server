f0d9e3faecacd65423730706e3723029b4972e12
Eldos <eldos.baktybekov@gmail.com>
2024-04-04 15:47:49 +0600

    :recycle: Refactor tasks

--- users/selectors/users.py
+++ users/selectors/users.py
@@ -1,8 +1,13 @@
+from collections.abc import Generator
+
+from django.db.models import QuerySet
+
 from users.exceptions import UserDoesNotExistsError
 from users.models import User
 
 __all__ = (
     'get_user_by_id',
+    'iter_users_with_birthdays',
 )
 
 
@@ -27,3 +32,24 @@ def get_user_by_id(user_id: int) -> User:
         )
     except User.DoesNotExist:
         raise UserDoesNotExistsError(user_id=user_id)
+
+
+def iter_users_with_birthdays(
+        *,
+        limit: int = 50,
+) -> Generator[QuerySet[User], None, None]:
+    offset: int = 0
+
+    while True:
+        users_with_birthdays = User.objects.exclude(born_on__isnull=True)
+
+        ordered_by_id = users_with_birthdays.order_by('id')
+
+        limited_users = ordered_by_id[offset:offset + limit + 1]
+
+        yield limited_users[:limit]
+
+        if len(limited_users) < limit:
+            break
+
+        offset += limit
