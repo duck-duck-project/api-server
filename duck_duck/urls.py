c8ac5580d39268d0c473034249f809ae34eb34c9
Eldos <eldos.baktybekov@gmail.com>
2024-06-20 15:53:33 +0600

    :sparkles: Include `user_characteristics` app's urls

--- duck_duck/urls.py
+++ duck_duck/urls.py
@@ -2,15 +2,18 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
+ROOT_PATH = ''
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('users.urls')),
-    path('', include('secret_messages.urls')),
-    path('economics/', include('economics.urls')),
-    path('', include('food_menu.urls')),
-    path('holidays/', include('holidays.urls')),
-    path('quizzes/', include('quizzes.urls')),
-    path('mining/', include('mining.urls')),
+    path(f'{ROOT_PATH}admin/', admin.site.urls),
+    path(f'{ROOT_PATH}', include('users.urls')),
+    path(f'{ROOT_PATH}', include('secret_messages.urls')),
+    path(f'{ROOT_PATH}economics/', include('economics.urls')),
+    path(f'{ROOT_PATH}', include('food_menu.urls')),
+    path(f'{ROOT_PATH}holidays/', include('holidays.urls')),
+    path(f'{ROOT_PATH}quizzes/', include('quizzes.urls')),
+    path(f'{ROOT_PATH}mining/', include('mining.urls')),
+    path(f'{ROOT_PATH}', include('user_characteristics.urls')),
 ]
 
 if settings.DEBUG:
