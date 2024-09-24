de6a65c1978a2907ce9f9de3b5910af446ba4f11
Eldos <eldos.baktybekov@gmail.com>
2024-09-24 21:35:29 +0600

    :sparkles: Add response body to break up API endpoint

--- relationships/selectors.py
+++ relationships/selectors.py
@@ -12,6 +12,7 @@ __all__ = (
     'has_active_relationship',
     'RelationshipCreateResultDTO',
     'get_active_relationship',
+    'get_relationship_by_id',
 )
 
 
@@ -34,12 +35,47 @@ class ActiveRelationshipDTO:
     next_level_experience_threshold: int
 
 
+@dataclass(frozen=True, slots=True)
+class RelationshipDTO:
+    first_user: UserPartialDTO
+    second_user: UserPartialDTO
+    created_at: datetime
+    level: int
+    broke_up_at: datetime
+
+
 def has_active_relationship(user_id: int) -> bool:
     relationship = Relationship.objects.filter(
         Q(first_user_id=user_id) | Q(second_user_id=user_id),
         broke_up_at__isnull=True,
     )
     return relationship.exists()
+
+
+def get_relationship_by_id(relationship_id: int) -> RelationshipDTO:
+    relationship = (
+        Relationship.objects
+        .select_related('first_user', 'second_user')
+        .only(
+            'first_user__id',
+            'first_user__fullname',
+            'first_user__username',
+            'second_user__id',
+            'second_user__fullname',
+            'second_user__username',
+            'experience',
+            'created_at',
+        )
+        .get(id=relationship_id)
+    )
+
+    return RelationshipDTO(
+        first_user=map_user_to_partial_dto(relationship.first_user),
+        second_user=map_user_to_partial_dto(relationship.second_user),
+        created_at=relationship.created_at,
+        broke_up_at=relationship.broke_up_at,
+        level=relationship.level,
+    )
 
 
 def get_active_relationship(user_id: int) -> ActiveRelationshipDTO:
@@ -57,6 +93,7 @@ def get_active_relationship(user_id: int) -> ActiveRelationshipDTO:
             'second_user__id',
             'second_user__fullname',
             'second_user__username',
+            'experience',
             'created_at',
         )
         .first()
--- relationships/views/relationships/break_up.py
+++ relationships/views/relationships/break_up.py
@@ -1,18 +1,30 @@
-from rest_framework import status
+from rest_framework import serializers
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from relationships.selectors import get_active_relationship
+from relationships.selectors import (
+    get_active_relationship,
+    get_relationship_by_id,
+)
 from relationships.services import break_up
+from users.serializers import UserPartialSerializer
 
 __all__ = ('RelationshipBreakUpApi',)
 
 
 class RelationshipBreakUpApi(APIView):
 
+    class OutputSerializer(serializers.Serializer):
+        first_user = UserPartialSerializer()
+        second_user = UserPartialSerializer()
+        created_at = serializers.DateTimeField()
+        broke_up_at = serializers.DateTimeField()
+        level = serializers.IntegerField()
+
     def delete(self, request: Request, user_id: int) -> Response:
         relationship = get_active_relationship(user_id)
         break_up(relationship.id)
-
-        return Response(status=status.HTTP_200_OK)
+        relationship = get_relationship_by_id(relationship.id)
+        serializer = self.OutputSerializer(relationship)
+        return Response(serializer.data)
