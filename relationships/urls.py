b2ddaa9112f87158a83dbde78e7995c8c4f5a835
Eldos <eldos.baktybekov@gmail.com>
2024-09-24 18:55:21 +0600

    :sparkles: Implement break up API endpoint

--- relationships/urls.py
+++ relationships/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 
-from relationships.views import RelationshipCreateApi, RelationshipRetrieveApi
+from relationships.views import (
+    RelationshipCreateApi,
+    RelationshipRetrieveApi,
+    RelationshipBreakUpApi,
+)
 
 urlpatterns = [
     path(
@@ -12,5 +16,10 @@ urlpatterns = [
         r'users/<int:user_id>/',
         RelationshipRetrieveApi.as_view(),
         name='relationship-retrieve',
+    ),
+    path(
+        r'users/<int:user_id>/break-up/',
+        RelationshipBreakUpApi.as_view(),
+        name='relationship-break-up',
     ),
 ]
